bc71ba1d7bf93d909747a0aebc25b4b05bb52c5d
Łukasz Kozłowski <ryuke12@gmail.com>
2020-07-31 15:55:58 +0200

    Add Redux Saga for logout

---
+++ src/store/sagas/auth.js
@@ -0,0 +1,16 @@
+// import { delay } from 'redux-saga';
+import { put, delay } from 'redux-saga/effects';
+
+import * as actions from '../actions';
+
+export function* logoutSaga(action) {
+  yield localStorage.removeItem('token');
+  yield localStorage.removeItem('expirationDate');
+  yield localStorage.removeItem('userId');
+  yield put(actions.logoutSucceed());
+}
+
+export function* checkAuthTimeoutSaga(action) {
+  yield delay(action.expirationTime * 1000);
+  yield put(actions.logout());
+}
