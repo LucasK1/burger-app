df16fbce720d8b30b6d2728b2ea58200509863b1
Łukasz Kozłowski <ryuke12@gmail.com>
2020-08-03 15:43:56 +0200

    Updates

---
+++ precache-manifest.1796db12da27d89ec09537f675ffc552.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8831d3a47f3e08ed1071348535f166e6",
+    "url": "/burger-app/index.html"
+  },
+  {
+    "revision": "328091dfbe79b3fe79c3",
+    "url": "/burger-app/static/css/3.db6547de.chunk.css"
+  },
+  {
+    "revision": "4493773b0555668c5e3e",
+    "url": "/burger-app/static/css/4.665b7c16.chunk.css"
+  },
+  {
+    "revision": "9937a3cd32dd980e3422",
+    "url": "/burger-app/static/css/5.9ab23c5d.chunk.css"
+  },
+  {
+    "revision": "5b1ad4a6236988dd467e",
+    "url": "/burger-app/static/css/main.39596798.chunk.css"
+  },
+  {
+    "revision": "b9d031464ba246b63fe9",
+    "url": "/burger-app/static/js/2.267223dc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/burger-app/static/js/2.267223dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "328091dfbe79b3fe79c3",
+    "url": "/burger-app/static/js/3.7afe0e23.chunk.js"
+  },
+  {
+    "revision": "4493773b0555668c5e3e",
+    "url": "/burger-app/static/js/4.db81e336.chunk.js"
+  },
+  {
+    "revision": "9937a3cd32dd980e3422",
+    "url": "/burger-app/static/js/5.7864a6cd.chunk.js"
+  },
+  {
+    "revision": "5b1ad4a6236988dd467e",
+    "url": "/burger-app/static/js/main.c891681c.chunk.js"
+  },
+  {
+    "revision": "abc04e9142308001cd2e",
+    "url": "/burger-app/static/js/runtime-main.cf4b5a1d.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-app/static/media/original.b8503d26.png"
+  }
+]);
